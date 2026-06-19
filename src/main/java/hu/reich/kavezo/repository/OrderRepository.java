34b09c841892d8858a720327d5f00b45396b0efd
Reich Mate <reichmate21@gmail.com>
2026-06-19 14:00:07 +0300

    Order es Product Repository kiterjesztese

---
+++ src/main/java/hu/reich/kavezo/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package hu.reich.kavezo.repository;
+
+import hu.reich.kavezo.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
