ea704df1078cfbeaa5e39f98e41c5d6a63dca715
Reich Mate <reichmate21@gmail.com>
2026-06-19 13:48:20 +0300

    Application megirasa/letrehozasa

---
+++ src/main/java/hu/reich/kavezo/KavezoApplication.java
@@ -0,0 +1,12 @@
+package hu.reich.kavezo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KavezoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KavezoApplication.class, args);
+    }
+}
