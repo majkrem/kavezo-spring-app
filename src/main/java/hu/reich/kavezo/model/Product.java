c92b4fa95b5d3975ebc667f449ab393df91bf629
Reich Mate <reichmate21@gmail.com>
2026-06-19 14:22:21 +0300

    product getter es setterek megiirasa

--- src/main/java/hu/reich/kavezo/model/Product.java
+++ src/main/java/hu/reich/kavezo/model/Product.java
@@ -27,4 +27,23 @@ public class Product {
         this.price = price;
         this.category = category;
     }
+
+    // Getters & Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public List<Order> getOrders() { return orders; }
+    public void setOrders(List<Order> orders) { this.orders = orders; }
 }
